b2c33ca7d64bd669a68de923ee0365cb5f78d103
Igor Boyko <iboyko@gmail.com>
2022-05-24 10:43:51 -0700

    First commit

--- server/app.js
+++ server/app.js
@@ -1,10 +1,13 @@
 // import files and packages up here
-
+const express = require('express');
 
 // create your express server below
-var app;
+const app = express();
 
 // add your routes and middleware below
+app.get('/', function(req, res){
+    res.status(200).send('New Express server');
+});
 
 // finally export the express application
 module.exports = app;
