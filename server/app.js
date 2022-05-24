8b7e7149db97f6e2567325602cb7dbd0be240f83
Igor Boyko <iboyko@gmail.com>
2022-05-24 11:48:22 -0700

    added server functionality

--- server/app.js
+++ server/app.js
@@ -1,12 +1,27 @@
 // import files and packages up here
 const express = require('express');
+const morgan = require('morgan');
 
 // create your express server below
 const app = express();
 
 // add your routes and middleware below
-app.get('/', function(req, res){
-    res.status(200).send('New Express server');
+app.use(morgan('dev'));
+
+var requestCount = 0;
+app.use(function(req, res, next) {
+    requestCount++;
+    console.log('Request number: ' + requestCount);
+    next();
+});
+
+app.get('/', function(req, res) {
+    res.status(200).send('This is the / route');
+});
+
+const data = require('./data.json');
+app.get('/data', function(req, res){
+    res.json(data);
 });
 
 // finally export the express application
